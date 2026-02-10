af7ada71df8be749899ef2e0af64d1f4e6117316
Jules <jules.lhp@gmail.com>
2026-02-10 16:25:23 -0500

    started to work on ui elements that shows when destroying an ovni

---
+++ Assets/Scripts/LiveTargetPoints.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class LiveTargetPoints : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject liveScoreGO;
+
+    [SerializeField]
+    private TextMeshProUGUI liveScoreTxt;
+
+    [SerializeField]
+    private TextMeshProUGUI liveScoreShadow;
+
+    [SerializeField]
+    private float upwardVelocity;
+
+    [SerializeField]
+    private float alphaTime;
+
+    private void OnEnable()
+    {
+        print(liveScoreTxt.alpha);
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        transform.Translate(Vector3.up * upwardVelocity * Time.deltaTime);
+
+        float alpha = Mathf.MoveTowards(liveScoreTxt.alpha, 0f, alphaTime); 
+
+        liveScoreTxt.alpha = alpha;
+        liveScoreShadow.alpha = alpha;
+
+        if (alpha <= 0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
